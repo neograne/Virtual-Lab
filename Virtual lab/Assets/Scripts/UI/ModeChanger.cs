5a33de1db5265106f217208e310300d6f45c7b28
neograne <neograneforlife@hotmail.com>
2025-12-06 21:41:45 +0500

    fix: ui tweaks; add: mode changer

---
+++ Virtual lab/Assets/Scripts/UI/ModeChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ModeChanger : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown modeDropDown;
+    public string currentMode = "Одноволновой";
+
+    private void Awake()
+    {
+        if (modeDropDown != null)
+            modeDropDown.onValueChanged.AddListener(OnSetMode); 
+    }
+
+    private void Start()
+    {
+        modeDropDown.ClearOptions();
+        List<string> modes = new List<string>() { "Одноволновой", "Двухволновой" };
+        modeDropDown.AddOptions(modes);
+    }
+
+
+    private void OnSetMode(int modeIndex)
+    {
+        if (modeIndex == 0)
+            Debug.Log("Текущий режим Одноволновой");
+        else if (modeIndex == 1)
+            Debug.Log("Текущий режим Двухволновой");
+    }
+}
---
+++ Virtual lab/Assets/Scripts/UI/ModeEnum.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ModeEnum : MonoBehaviour
+{
+    enum Modes
+    {
+        Одноволновой,
+        Двухволновой
+    }
+}
