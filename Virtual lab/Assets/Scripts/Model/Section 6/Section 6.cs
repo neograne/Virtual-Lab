1a691e1f6b75471835e39a8830fea627d185b67f
neograne <neograneforlife@hotmail.com>
2025-12-16 22:29:08 +0500

    add: section 6 script

---
+++ Virtual lab/Assets/Scripts/Model/Section 6/Section 6.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Section6 : MonoBehaviour
+{
+    [SerializeField] private GameObject section6;
+    [SerializeField] private float minAngle = 0;
+    [SerializeField] private float maxAngle = 360;
+    [SerializeField] private Vector2 mouseTurn;
+    [SerializeField] public float trueAngle;
+
+    private void OnMouseDrag()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mouseTurn.x -= Input.GetAxis("Mouse X");
+            mouseTurn.x = Mathf.Clamp(mouseTurn.x, minAngle, maxAngle);
+            trueAngle = mouseTurn.x;
+            section6.transform.localRotation = Quaternion.Euler(0, mouseTurn.x, 0);
+        }
+    }
+
+    public void GlobalReset()
+    {
+
+    }
+}
