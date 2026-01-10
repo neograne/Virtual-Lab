c710c0ff359ce3cdaac7d682b42fc2b2a9e4fe58
neograne <neograneforlife@hotmail.com>
2026-01-10 23:03:58 +0500

    rework: enhancer buttons

---
+++ Virtual lab/Assets/Scripts/Model/Enhancer/EnhancerButtonZero.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnhancerButtonZero : MonoBehaviour
+{
+    [SerializeField] private GameObject button;
+    private bool isFirstClick = true;
+
+    public void OnMouseDown()
+    {
+        if (isFirstClick)
+        {
+            Debug.Log("Режим установки нуля включен");
+            isFirstClick = false;
+            button.transform.localRotation = Quaternion.Euler(0, 0, -45);
+        }
+        else
+        {
+            Debug.Log("Режим установки нуля выключен");
+            isFirstClick = true;
+            button.transform.localRotation = Quaternion.Euler(0, 0, 45);
+        }
+    }
+
+    public void GlobalReset()
+    {
+        isFirstClick = true;
+        button.transform.localRotation = Quaternion.Euler(0, 0, 45);
+    }
+}
