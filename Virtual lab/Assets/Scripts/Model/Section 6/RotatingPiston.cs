b1ca2a34fd88ac21dd4bd402d3036c2219cc752d
neograne <neograneforlife@hotmail.com>
2026-01-13 19:03:04 +0500

    add: new model section6 + interactions

--- Virtual lab/Assets/Scripts/Model/Section 6/RotatingPiston.cs
+++ Virtual lab/Assets/Scripts/Model/Section 6/RotatingPiston.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 
 public class RotatingPiston : MonoBehaviour
 {
@@ -6,8 +7,9 @@ public class RotatingPiston : MonoBehaviour
     [SerializeField] private GameObject rotatingPiston;
 
     [Header("Ограничения движения")]
-    [SerializeField] private float minPosition = 0.0208f;
-    [SerializeField] private float maxPosition = 0.0263f;
+    [SerializeField] private float minPosition = 0.02067f;
+    [SerializeField] private float maxPosition = 0.02661f;
+    [SerializeField] private float unityRange;
     [SerializeField] private float maxRotationAngle = 360f;
 
     [Header("Настройки чувствительности")]
@@ -17,6 +19,8 @@ public class RotatingPiston : MonoBehaviour
     [Header("DEBUG")]
     [SerializeField] private float currentPosition;
     [SerializeField] private float currentRotation;
+    [SerializeField] private float fineSensitivity = 0.001f;
+    [SerializeField] private float normalSensitivity = 1f;
 
     public float truePosition;
 
@@ -33,20 +37,26 @@ public class RotatingPiston : MonoBehaviour
     {
         if (!isDragging) return;
 
+        float sens = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)
+                ? fineSensitivity
+                : normalSensitivity;
+        unityRange = maxPosition - minPosition;
+
         Vector3 currentMousePosition = Input.mousePosition;
         float deltaX = currentMousePosition.x - lastMousePosition.x;
 
-        currentPosition += deltaX * moveSensitivity * 0.0001f;
+        currentPosition += deltaX * sens * moveSensitivity * 0.0001f;
         currentPosition = Mathf.Clamp(currentPosition, minPosition, maxPosition);
 
-        currentRotation += deltaX * rotationSensitivity * 0.01f;
+        currentRotation += deltaX * sens * rotationSensitivity * 0.01f;
         currentRotation = Mathf.Clamp(currentRotation, 0f, maxRotationAngle);
 
         rotatingPiston.transform.localPosition = new Vector3(rotatingPiston.transform.localPosition.x, currentPosition, rotatingPiston.transform.localPosition.z);
         rotatingPiston.transform.localRotation = Quaternion.Euler(0, currentRotation, 0);
 
         lastMousePosition = currentMousePosition;
-        truePosition = (currentPosition - 0.0208f) / 0.0055f * 0.07f;
+        truePosition = (currentPosition - minPosition) / unityRange * 0.07f;
+        truePosition = MathF.Round(truePosition, 3);
     }
 
     private void OnMouseUp()
